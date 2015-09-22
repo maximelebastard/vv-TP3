754fb7dce974bad2e0be8b1bd11c84e6688c5819
Jose A. Galindo <jagalindo@us.es>
2015-09-22 14:15:07 +0200

    adding some helper classes

---
+++ Suport/Pmd-bin-5.0.5/testsimpleGame.sh
@@ -0,0 +1,2 @@
+bin/run.sh pmd -dir ../../../vv-TP2/Stubsmocks/simpleGame/src/main/java/ -f html -R java-basic,java-design,java-unusedcode,java-optimizations,java-empty,java-strictexception,java-strings > mytest.html
+
